de8eaa14887757f5e517889d2276ab2b33133a22
Jeffrey M Moore <jmoore@manifold.ai>
2021-01-21 19:40:13 -0800

    feat: added rc4me data structure for organizing repos (#7)
    * Try and add CLI
    
    * Get executable running
    
    * feat: added rc4me data structure for organizing repos
    
    * docs: remove dead comments, fix variable references in docstrings
    
    Co-authored-by: Michael Stefferson <mstefferson@gmail.com>

--- rc4me/run.py
+++ rc4me/run.py
@@ -1,30 +1,53 @@
 """TODO - Docstring."""
 
+import sys
 from pathlib import Path
 from typing import Optional
 import logging
 import click
 
-from rc4me.fetch_rcs import fetch_rcs
-from rc4me.util import copyfiles
+from rc4me.util import RcDirs, link_files
+from rc4me.prepare_repo import prepare_repo
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-RC4ME_HOME = Path.home() / ".rc4me"
-
 
 @click.command()
-@click.argument("repo", type=str)
-@click.argument("dest", required=False, type=str)
-def cli(repo: str, dest: Optional[str] = None) -> None:
+# Allow calling function without an argument for --revert or --reset options
+@click.argument("repo", required=False, type=str)
+@click.option("--dest", type=click.Path())
+@click.option("--home", type=click.Path())
+@click.option("--revert", is_flag=True)
+@click.option("--reset", is_flag=True)
+def cli(
+    repo: Optional[str] = None,
+    home: Optional[str] = None,
+    dest: Optional[str] = None,
+    revert: bool = False,
+    reset: bool = False,
+) -> None:
     """Fetch dotfile repo and link rc files to destination."""
-    # Check if the config is already downloaded, and if not, clone to rc4me home dir
-    fetch_rcs(RC4ME_HOME, repo)
-    # Copy rc4me target config to destination (assume home directory for now)
+    # If the command was called without any arguments or options
+    if repo is None and not (revert or reset):
+        # TODO--Show user help
+        logger.warning(f"Usage: {sys.argv[0]} [options] <repo_name>")
+        sys.exit(1)
+    # Set up defaults
+    if home is None:
+        home = Path.home() / ".rc4me"
+    else:
+        home = Path(home)
     if dest is None:
-        dest = str(Path.home())
-    copyfiles(RC4ME_HOME / repo, dest)
+        dest = Path.home()
+    else:
+        dest = Path(dest)
+    # Init rc4me directory variables
+    rc_dirs = RcDirs(repo, home, dest)
+    # Clone repo to rc4me home dir or update existing local config repo
+    prepare_repo(rc_dirs, revert, reset)
+    # Link rc4me target config to destination
+    link_files(rc_dirs)
 
 
 if __name__ == "__main__":
--- rc4me/util.py
+++ rc4me/util.py
@@ -1,37 +1,129 @@
+"""Utility classes and functions for rc4me."""
+
 import logging
 import shutil
+from typing import Optional
 from pathlib import Path
+
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-def copyfiles(source_dir: str, destination_dir: str):
-    """Copy files from source to (hidden) destination
+class RcDirs:
+    """Class for storing and manipulating rc4me home directory structure."""
 
-    Example filesystem:
-        dir1/
-            bashrc
+    def __init__(
+        self, target_config: Optional[str], rc4me_home: Path, rc4me_dest: Path
+    ):
+        """Initialize paths to home and source rc4me config repos.
 
-    copyfiles("dir1", "dir2")
+        Creates attributes `source`, which is the path to the
+        local config repo that will be copied to the user's target
+        directory.
 
-    Output:
-        dir1/
-            bashrc
-        dir2/
-            .bashrc
+        Args:
+            target_config: Path to local rc repo or remote rc repo name. If
+                None, home variables are initialized, but no source repo is
+                defined.
+            rc4me_home: Path to rc4me home directory.
+            rc4me_dest: Directory to copy rc files to.
+        """
+        # Git/local repo from which to clone rc config
+        self.target = target_config
+        # Directory that holds all cloned rc config repos, and init, prev, current
+        self.home = rc4me_home
+        # Directory to copy rc4me files to (e.g. $HOME)
+        self.dest = rc4me_dest
+        # Init rc4me home dir variables (init, prev, current)
+        self._init_rc4me_home()
+        if self.target is None:
+            return
+        self.target_is_local = Path(self.target).expanduser().exists()
+        # Init self.source attribute, which will be the path to local repo in
+        # rc4me_home with target config
+        if self.target_is_local:
+            self.target = str(Path(self.target).expanduser())
+            self.source = self.home / Path(self.target).name
+        else:
+            self.source = self.home / self.target
+
+    def relink_current_to(self, target: Path):
+        """Change current symlink to point to target path."""
+        # Update prev symlink to point to current
+        self.prev.unlink()
+        self.prev.symlink_to(self.current.resolve())
+        # Update current to point to target
+        self.current.unlink()
+        self.current.symlink_to(target)
+
+    def _init_rc4me_home(self):
+        """Create rc4me directory variables w/ init, prev, and current config.
+
+        This function creates the rc4me home directory path
+        variables and scaffolds the directory if necessary.  The
+        `init` directory should contain all non-symlinked files
+        that would otherwise be overwritten by rc4me. The `prev`
+        and `current` are symlinked directories that point to the
+        user's previous and current rc4me config directories,
+        respectively, initially linked to `init` at creation.
+        """
+        self.init = self.home / "init"
+        self.prev = self.home / "prev"
+        self.current = self.home / "current"
+        # If this is the first time calling rc4me, scaffold rc4me home dir
+        if not self.init.exists():
+            # Allow this to fail if home parent dir doesn't
+            # exist (the home dir itself may already exist if
+            # the user specified --home with an existing dir)
+            self.home.mkdir(exist_ok=True)
+            self.init.mkdir()
+            self.prev.symlink_to(
+                self.init,
+            )
+            self.current.symlink_to(self.init)
+
+
+def link_files(rc_dirs: RcDirs):
+    """Link files from rc4me source to (hidden) destination.
+
+    Transfers files found in the rc4me source directory and
+    creates symlinks of them in the rc4me destination
+    directory. If the source directory is rc4me_home/init,
+    the files are copied instead, allowing a user to safely
+    delete their rc4me home dir after a reset.
 
     Args:
-        source_dir: source directory
-        destination_dir: destination directory
+        rc_dirs: Data structure with rc4me home directory organization variables.
     """
-    source_path = Path(source_dir)
-    destin_path = Path(destination_dir)
+    source_path = rc_dirs.current
+    destin_path = rc_dirs.dest
+    # If we are restoring the initial config, copy the files rather than
+    # link them, so that the user can safely delete their rc4me home dir
+    copy_files = source_path.resolve() == rc_dirs.init
     logger.info(f"Moving files from {source_path} to {destin_path}")
     for f in source_path.glob("*"):
-        # Skip copying the .git directory and README file (stop-gap)
-        if f.name == ".git" or "README" in f.name:
+        # Skip copying any directories or README files for now (stop-gap)
+        # TODO -- add ability to copy/link directories
+        if f.is_dir() or "README" in f.name:
             continue
         new_path = destin_path / f".{f.name}"
-        logger.info(f"Moving {f}->{new_path}")
-        shutil.copy(f, new_path)
+        # Unlink any files that exist at new_path
+        if new_path.exists():
+            # If we would be overwriting a non-symlinked file, copy to rc4me_home/init
+            if not new_path.is_symlink():
+                backup_path = rc_dirs.init / f"{f.name}"
+                logger.info(f"Backing up {new_path}->{backup_path}")
+                shutil.copy(new_path, backup_path)
+            # Unlink the existing file
+            new_path.unlink()
+        if copy_files:
+            # Copy the files from the source to the new path -- only occurs
+            # when source is rc4me_home/init
+            logger.info(f"Moving {f}->{new_path}")
+            shutil.copy(f, new_path)
+        else:
+            # Symlink the source rc files to the new path (syntax on this is
+            # opposite the copy syntax, which may lead to confusion...)
+            logger.info(f"Linking {new_path}->{f}")
+            new_path.symlink_to(f)
